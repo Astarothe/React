2092d36c8f5df4aa6f4701af06edc878823b021f
Astaroth <minatosavich@gmail.com>
2021-05-08 19:36:14 +0300

    feat: components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://www.pinclipart.com/picdir/big/546-5464333_icon-aperture-aperture-science-logo-clipart.png"
+                alt=""/>
+        </header>
+    )
+}
+
+export default  Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://img.17qq.com/images/fhhhhhswpqy.jpeg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
